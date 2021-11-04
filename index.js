686634810c07bc6cc882c4266bcb87e31b4f8aec
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-11-04 18:47:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('NzY1MjIxOTk0ODg2NzI1NjUy.X4RqhA._494wtKFoRFumuBHgT5g0mqTDEA');
+client.login('');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
